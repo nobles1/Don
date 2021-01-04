9adfd4a44975e310f894649fa82712c64d375e0e
nobles1 <blakenoblesyear@gmail.com>
2021-01-04 10:42:54 -0500

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1100},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-music-page","displayName":"Music Page","link":{"linkType":"LinkTypePage","href":"#!page-music-page"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-our-mission","displayName":"Our Mission","link":{"linkType":"LinkTypePage","href":"#!page-our-mission"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-news-press","displayName":"News & Press","link":{"linkType":"LinkTypePage","href":"#!page-news-press"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-how-to-help","displayName":"How to Help","link":{"linkType":"LinkTypePage","href":"#!page-how-to-help"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+/* slider buttons */
+$(function() {
+    try {
+        var slider = $('.slider .unslider').children().get(0);
+        var unslider = $(slider).data('unslider');
+        $('.sub-nav').click(function() {
+            unslider.next()
+            $('.sub-nav-active').removeClass('sub-nav-active');
+            $(this).addClass('sub-nav-active');
+        });
+    }
+    catch(e) {
+    }
+})
